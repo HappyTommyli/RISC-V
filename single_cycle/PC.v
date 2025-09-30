e69588cd46d380d39d3f22849c133c0c476d75ac
ScasLH <xiuwudi@gmail.com>
2025-09-30 15:56:19 +0800

    pc

--- single_cycle/PC.v
+++ single_cycle/PC.v
@@ -1,1 +1,11 @@
-
+module pc (input wire clk, input wire rst, input wire [31:0] next_pc, output reg [31:0] pc_address
+) (
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            pc_address <= 32'b0;
+        end else begin
+            pc_address <= next_pc;
+        end
+    end
+);  
+endmodule
