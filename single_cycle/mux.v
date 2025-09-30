e389e4604f7a532a539d3e234b95cb79090758cc
ScasLH <xiuwudi@gmail.com>
2025-09-30 17:39:27 +0800

    mux

--- single_cycle/mux.v
+++ single_cycle/mux.v
@@ -1,1 +1,13 @@
-
+module mux(
+    input wire [31:0] in0,
+    input wire [31:0] in1,
+    input wire ctrl,
+    output reg [31:0] ou
+)
+    always @(*) begin
+        if (ctrl) begin
+            out = in1;
+        end else begin
+            out = in0;
+        end
+    end
