ab757d5e676a9182aa97979e39332bc728f144cd
ScasLH <164500828+ScasLH@users.noreply.github.com>
2025-10-01 17:46:48 +0800

    Refactor mux module for cleaner output assignment

--- single_cycle/mux.v
+++ single_cycle/mux.v
@@ -1,13 +1,10 @@
 module mux(
-    input wire [31:0] in0,
-    input wire [31:0] in1,
-    input wire ctrl,
-    output reg [31:0] ou
-)
+    input wire [31:0] in0, // first input
+    input wire [31:0] in1, // second input
+    input wire ctrl,    // control signal
+    output reg [31:0] out // output
+);
     always @(*) begin
-        if (ctrl) begin
-            out = in1;
-        end else begin
-            out = in0;
-        end
+        out = ctrl ? in1 : in0;
     end
+endmodule
