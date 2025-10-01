9514feb78cc7f04f78bc84f959088e336fd15f03
HappyTommy <2522398080@qq.com>
2025-10-01 19:51:28 +0800

    pc_update

--- single_cycle/PC_update.v
+++ single_cycle/PC_update.v
@@ -1,7 +1,31 @@
 module PC_update (
-    input [31:0] rs1_data, input jump;input branch, input [31:0]pc_address,input [31:0]imm,input zero,output reg next_pc
+    input [31:0] rs1_data,
+    input jump,
+    input branch,
+    input [31:0]pc_address,
+    input [31:0]imm,
+    input zero,
+    output reg [31:0]next_pc
   );
 
+  always @( *)
+  begin
+    if (jump)
+    begin
+      next_pc = pc_address + imm;
+    end
+
+    else if (branch && zero)
+    begin
+      next_pc = pc_address + imm;
+    end
+
+    else
+    begin
+      next_pc = pc_address + 32'b100; //PC+4
+    end
+
+  end
 
 
 endmodule
