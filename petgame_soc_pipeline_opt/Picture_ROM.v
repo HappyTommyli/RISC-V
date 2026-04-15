f5ed10516bdc16d24492f46701b4110b0b0c792f
ScasLH <164500828+ScasLH@users.noreply.github.com>
2026-04-15 15:26:38 +0800

    Update Picture_ROM.v

--- petgame_soc_pipeline_opt/Picture_ROM.v
+++ petgame_soc_pipeline_opt/Picture_ROM.v
@@ -1,14 +1,20 @@
 module Picture_ROM (
     input  wire        clk,
-    input  wire [11:0] addr, // 12 張圖 * 128 Byte = 1536
+    input  wire [11:0] addr, 
     output reg  [7:0]  dout
 );
-    localparam ROM_DEPTH = 1536;
+    // 定義 ROM 空間
+    reg [7:0] rom [0:383]; 
 
-    (* rom_style = "block" *) reg [7:0] rom [0:ROM_DEPTH-1];
-
-    // 使用 include 載入數據
+    // 這裡放入 initial 區塊
     initial begin
+        // 先將所有空間清零，防止出現未知態 (X)
+        integer i;
+        for (i = 0; i < 384; i = i + 1) begin
+            rom[i] = 8'h00;
+        end
+
+        // 引入純數據賦值的檔案
         `include "picture_rom_init.v"
     end
 
